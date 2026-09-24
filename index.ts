0f64f64e6aa3305bcc5baffe5ae22af38d17ce23
sandesh shahi <devsanbid@gmail.com>
2026-09-24 16:52:44 +0545

    dynamic change

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import express from "express";
 
-const app = express()
+const app = express();
 
-app.get("/health", (_, res) => {
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("My Node API is running!");
+});
+
+app.get("/health", (req, res) => {
   res.status(200).json({
     message: "Server is healthy!!",
-    status: 200 
-  })
-} )
+    status: 200
+  });
+});
 
-app.listen(3000, ()=> {
-  console.log("3000 PORT is running.......")
-})
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
